cdc7130bfae239529b3dbddafcc8e0af55e73f1d
Adelon Zeta <adelonzeta@Adelons-MacBook-Air.local>
2018-01-18 11:47:06 -0800

    desktop styles

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,21 @@
-import 'jquery'
+import $ from 'jquery'
 import 'popper.js'
 import 'bootstrap'
+import 'jquery-smooth-scroll'
+
+$(document).ready(function() {
+  var nav = $('#navigation')
+
+  $(window).scroll(function() {
+    var windowPosition = $(window).scrollTop()
+
+    if (windowPosition > 0) {
+      nav.addClass('navigation--inverse')
+    }
+    else {
+      nav.removeClass('navigation--inverse')
+    }
+  })
+})
+
+$('.scroll-link').smoothScroll({ offset: -112 })
